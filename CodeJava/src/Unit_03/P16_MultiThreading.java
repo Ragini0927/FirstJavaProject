7700fc1d98fb0219a5034828f171672b0bcc3dd1
HP <HP@LAPTOP-QPELPC50>
2022-05-27 09:11:00 +0530

    new commit

--- CodeJava/src/Unit_03/P16_MultiThreading.java
+++ CodeJava/src/Unit_03/P16_MultiThreading.java
@@ -1,4 +1,5 @@
 package Unit_03;
+import java.util.Scanner;
 /*
  * MULTITHREADING  PREQUISITES
  * 
@@ -15,12 +16,23 @@ package Unit_03;
 public class P16_MultiThreading {
 
 	public static void main(String[] args) {
-		 abc2 obj=new abc2();
-		 obj.threadConcept();
-		 obj.threadSynchronization();
-		 obj.interThreadCommunication();
-		 obj.ProducerConsumerProblems();
-		 obj.Wait_And_Notify();
+//		 abc2 obj=new abc2();
+//		 obj.threadConcept();
+//		 obj.threadSynchronization();
+//		 obj.interThreadCommunication();
+//		 obj.ProducerConsumerProblems();
+//		 obj.Wait_And_Notify();
+//		c1 obj1=new c1();
+//		c2 obj2=new c2();
+//		obj1.show1();
+//		obj2.show2();	
+		Thread t1=new Thread(new T1(),"T1");
+		Thread t2=new Thread(new T2(),"T2");
+		Thread t3=new Thread(new T3(),"T3");
+		t1.start();
+		t2.start();
+		t3.start();
+		
   }
 }
 class abc2
@@ -39,5 +51,45 @@ class abc2
 	}
 	void Wait_And_Notify(){
 		
+	}
+}
+class c1{
+	void show1()
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		System.out.println(a);
+	}
+}
+class c2{
+	void show2()
+	{
+		for(int i=0;i<10;i++) {
+		System.out.println("Hey-2");
+		}
+	}
+}
+class T1 extends Thread{
+	public void run()
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		System.out.println(a);
+	}
+}
+class T2 extends Thread{
+	public void run()
+	{
+		for(int i=0;i<10;i++) {
+		System.out.println("Hey-2");
+		}
+	}
+}
+class T3 extends Thread{
+	public void run()
+	{
+		for(int i=0;i<10;i++) {
+		System.out.println("Hey-3");
+		}
 	}
 }
